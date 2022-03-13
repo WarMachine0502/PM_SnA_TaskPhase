5b0f916615b5da5df29738929a2d8f07bb9475f1
Sahasrad Thakur <69747465+WarMachine0502@users.noreply.github.com>
2022-03-13 20:37:00 +0530

    Reduced variables; added comments

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -1,167 +1,174 @@
-#include<iostream>
-#include<stdlib.h>
-#include<string>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
 using namespace std;
 
-char space[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+int row, column; //initialising variables for entering X or 0 by row,column pair
+char figure = 'X';
+bool tieStatus = false;
+string p1 = "Player 1 (X)";
+string p2 = "Player 2 (0)";
 
-int row, column;
-char token = 'X';
-bool tie=false;
-string n1="Player 1 (X)";
-string n2="Player 2 (0)";
+char grid[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}}; //Creates array for Tic Tac Toe Grid.
 
-void functionOne(){
-    cout<< "\n\n      |     |    \n";
-    cout<< "    "<< space[0][0]<<" |  "<<space[0][1]<< "  |  "<<space[0][2]<<"  \n";
-    cout<< "  ____|_____|____\n";
-    cout<< "      |     |    \n";
-    cout<< "    "<< space[1][0]<<" |  "<<space[1][1]<< "  |  "<<space[1][2]<<"  \n";
-    cout<< "  ____|_____|____\n";
-    cout<< "      |     |    \n";
-    cout<< "    "<< space[2][0]<<" |  "<<space[2][1]<< "  |  "<<space[2][2]<<"  \n";
-    cout<< "      |     |    \n\n";
-}
-
-void functionTwo(){
-    int digit;
-    if (token=='X')
-    {
-        cout<<n1<<", please enter cell number: ";
-        cin>>digit;
-    }
-
-if (token=='0')
-    {
-        cout<<n2<<", please enter cell number: ";
-        cin>>digit;
-    }
-
-    if(digit==1)
-    {
-        row=0;
-        column=0;
-    }
-    if(digit==2)
-    {
-        row=0;
-        column=1;
-    }
-    if(digit==3)
-    {
-        row=0;
-        column=2;
-    }
-    if(digit==4)
-    {
-        row=1;
-        column=0;
-    }
-    if(digit==5)
-    {
-        row=1;
-        column=1;
-    }
-    if(digit==6)
-    {
-        row=1;
-        column=2;
-    }
-    if(digit==7)
-    {
-        row=2;
-        column=0;
-    }
-    if(digit==8)
-    {
-        row=2;
-        column=1;
-    }
-    if(digit==9)
-    {
-        row=2;
-        column=2;
-    }
-    else if(digit<1 || digit>9){
-        cout<<"Invalid !"<<endl;
-    }
-
-    if(token=='X' && space[row][column]!='X' && space[row][column]!='0')
+void createGrid() //Function to render the Tic Tac Toe grid for each instance
 {
-    space[row][column]='X';
-    token='0';
-}
-else if(token=='0' && space[row][column]!='X' && space[row][column]!='0')
-{
-    space[row][column]='0';
-    token='X';
-}
-else{
-    cout<<"There is no empty space!"<<endl;
-    functionTwo();
-}
-functionOne();
+    cout << "\n\n      |     |    \n";
+    cout << "    " << grid[0][0] << " |  " << grid[0][1] << "  |  " << grid[0][2] << "  \n";
+    cout << "  ____|_____|____\n";
+    cout << "      |     |    \n";
+    cout << "    " << grid[1][0] << " |  " << grid[1][1] << "  |  " << grid[1][2] << "  \n";
+    cout << "  ____|_____|____\n";
+    cout << "      |     |    \n";
+    cout << "    " << grid[2][0] << " |  " << grid[2][1] << "  |  " << grid[2][2] << "  \n";
+    cout << "      |     |    \n\n";
 }
 
-bool functionThree()
+void enterFigure() //Asks for human input(cell number) where X or 0 is entered.
 {
-    for (int i=0;i<3;i++)
+    int cellNo;
+    if (figure == 'X')
     {
-        if(space[i][0]==space[i][1] && space[i][0]==space[i][2] || space[0][i]==space[1][i] && space[0][i]==space[2][i])
+        cout << p1 << ", please enter cell number: ";
+        cin >> cellNo;
+    }
+
+    if (figure == '0')
+    {
+        cout << p2 << ", please enter cell number: ";
+        cin >> cellNo;
+    }
+
+    if (cellNo == 1)
+    {
+        row = 0;
+        column = 0;
+    }
+    if (cellNo == 2)
+    {
+        row = 0;
+        column = 1;
+    }
+    if (cellNo == 3)
+    {
+        row = 0;
+        column = 2;
+    }
+    if (cellNo == 4)
+    {
+        row = 1;
+        column = 0;
+    }
+    if (cellNo == 5)
+    {
+        row = 1;
+        column = 1;
+    }
+    if (cellNo == 6)
+    {
+        row = 1;
+        column = 2;
+    }
+    if (cellNo == 7)
+    {
+        row = 2;
+        column = 0;
+    }
+    if (cellNo == 8)
+    {
+        row = 2;
+        column = 1;
+    }
+    if (cellNo == 9)
+    {
+        row = 2;
+        column = 2;
+    }
+    else if (cellNo < 1 || cellNo > 9)
+    {
+        cout << "Invalid !" << endl;
+    }
+
+    if (figure == 'X' && grid[row][column] != 'X' && grid[row][column] != '0') //checks if an already filled cell is referenced again by human; runs the function again.
+    {
+        grid[row][column] = 'X';
+        figure = '0';
+    }
+    else if (figure == '0' && grid[row][column] != 'X' && grid[row][column] != '0')
+    {
+        grid[row][column] = '0';
+        figure = 'X';
+    }
+    else
+    {
+        cout << "Cell is not empty, plz enter empty cell number!" << endl;
+        enterFigure();
+    }
+    createGrid();
+}
+
+
+bool checkStatus() //Function to check the hotizontal/vertical/diagonal rows to check for Winner, gives boolean values.
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2] || grid[0][i] == grid[1][i] && grid[0][i] == grid[2][i])
+            return true;
+    }
+
+    if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] || grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
+    {
         return true;
     }
 
-    if (space[0][0]==space[1][1] && space[1][1] == space[2][2] || space [0][2]==space[1][1] && space[1][1] == space[2][0]){
-        return true;
-    }
-
-    for(int i=0;i<3;i++)
+    for (int i = 0; i < 3; i++)
     {
-        for(int j=0;j<3;j++)
+        for (int j = 0; j < 3; j++)
         {
-            if (space[i][j]!='X' && space[i][j] != '0'){
+            if (grid[i][j] != 'X' && grid[i][j] != '0')
+            {
                 return false;
             }
         }
     }
-    tie=true;
+    tieStatus = true;
     return false;
-
 }
 
-void functionFour(){
-    if(token == 'X' && tie == false)
-    {
-        cout<<n2<<" wins!!"<<endl;
-    }
-    else if(token == '0' && tie==false)
-    {
-        cout<<n1<<" wins!!"<<endl;
-    }
-    else{
-        cout<<"It's a draw!"<<endl;
-    }
-}
-
-int main()
+void declare() //Function to declare the Game Winner based on the values returned from checkStatus() function.
 {
-    cout<<"TIC TAC TOE - Human vs Human";
-
-    cout<< "\n\n      |     |    \n";
-    cout<< "    "<< space[0][0]<<" |  "<<space[0][1]<< "  |  "<<space[0][2]<<"  \n";
-    cout<< "  ____|_____|____\n";
-    cout<< "      |     |    \n";
-    cout<< "    "<< space[1][0]<<" |  "<<space[1][1]<< "  |  "<<space[1][2]<<"  \n";
-    cout<< "  ____|_____|____\n";
-    cout<< "      |     |    \n";
-    cout<< "    "<< space[2][0]<<" |  "<<space[2][1]<< "  |  "<<space[2][2]<<"  \n";
-    cout<< "      |     |    \n\n";
-
-    while(!functionThree())
+    if (figure == 'X' && tieStatus == false)
     {
-        functionTwo();
-        functionThree();
+        cout << p2 << " wins!!" << endl;
     }
-    functionFour();
+    else if (figure == '0' && tieStatus == false)
+    {
+        cout << p1 << " wins!!" << endl;
+    }
+    else
+    {
+        cout << "It's a draw!" << endl;
+    }
+}
 
+int main() //Main Driver Method of Program
+{
+    cout << "TIC TAC TOE - Human vs Human";
+
+    cout << "\n\n      |     |    \n";
+    cout << "    " << grid[0][0] << " |  " << grid[0][1] << "  |  " << grid[0][2] << "  \n";
+    cout << "  ____|_____|____\n";
+    cout << "      |     |    \n";
+    cout << "    " << grid[1][0] << " |  " << grid[1][1] << "  |  " << grid[1][2] << "  \n";
+    cout << "  ____|_____|____\n";
+    cout << "      |     |    \n";
+    cout << "    " << grid[2][0] << " |  " << grid[2][1] << "  |  " << grid[2][2] << "  \n";
+    cout << "      |     |    \n\n";
+
+    while (!checkStatus())
+    {
+        enterFigure();
+        checkStatus();
+    }
+    declare();
 }
